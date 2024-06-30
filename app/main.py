44977d2fff3636da5569f67dd85fa18fa38f81de
Amin Saedi <amin.di.sam@gmail.com>
2024-06-30 14:57:48 -0400

    CLI tests (cli-test-1719773868284)

--- app/main.py
+++ app/main.py
@@ -7,8 +7,15 @@ def main():
     sys.stdout.flush()
 
     # Wait for user input
-    input()
+    input_str = input()
 
+    match input_str:
+        case "exit":
+            sys.exit(0)
+        case _:
+            sys.stdout.write(input_str + ": command not found\n")
+
+    sys.stdout.flush()
 
 if __name__ == "__main__":
     main()
