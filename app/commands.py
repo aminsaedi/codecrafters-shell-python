a81277355b84371fc61d16ccbdb8839b499ed396
Amin Saedi <amin.di.sam@gmail.com>
2024-06-30 15:42:02 -0400

    CLI tests (cli-test-1719776522745)

--- app/commands.py
+++ app/commands.py
@@ -1,4 +1,5 @@
 import sys
+import os
 
 def app_exit(args):
     sys.exit(0 if not args else int(args[0]))
@@ -9,8 +10,22 @@ def app_echo(args):
 app_echo.type = "shell builtin"
 
 def app_type(args):
+    executables = {}
+    try:
+        PATH = os.environ.get("PATH", "")
+        all_dirs = PATH.split(":")
+        print(all_dirs)
+        for dir in all_dirs:
+            for file in os.listdir(dir):
+                executables[file] = os.path.join(dir, file)
+    except:
+        pass
+
+
     if args[0] in commands:
         sys.stdout.write(args[0] + " is a " + commands[args[0]].type + "\n")
+    elif args[0] in executables:
+        sys.stdout.write(args[0] + " is " + executables[args[0]] + "\n")
     else:
         sys.stdout.write(args[0] + ": not found\n")
 app_type.type = "shell builtin"
