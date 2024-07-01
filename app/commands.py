48279ba7ec58185d96262fbe89cc92a3fee14d51
Amin Saedi <amin.di.sam@gmail.com>
2024-07-01 08:58:41 -0400

    Run a program #IP1

--- app/commands.py
+++ app/commands.py
@@ -1,5 +1,7 @@
 import sys
 import os
+import subprocess
+import shutil
 
 def app_exit(args):
     sys.exit(0 if not args else int(args[0]))
@@ -10,23 +12,21 @@ def app_echo(args):
 app_echo.type = "shell builtin"
 
 def app_type(args):
-    executables = {}
-    PATH = os.environ.get("PATH", "")
-    all_dirs = PATH.split(":")
-    for dir in all_dirs:
-        if os.path.isdir(dir):
-            for file in os.listdir(dir):
-                    if os.access(os.path.join(dir, file), os.X_OK):
-                        executables[file] = os.path.join(dir, file)
-
-
     if args[0] in commands:
         sys.stdout.write(args[0] + " is a " + commands[args[0]].type + "\n")
-    elif args[0] in executables:
-        sys.stdout.write(args[0] + " is " + executables[args[0]] + "\n")
+    elif path := shutil.which(args[0]):
+        sys.stdout.write(args[0] + " is " + path + "\n")
     else:
         sys.stdout.write(args[0] + ": not found\n")
 app_type.type = "shell builtin"
+
+
+def run_executable(command, args):
+    try:
+        subprocess.run([command] + args)
+    except FileNotFoundError:
+        sys.stdout.write(command + ": command not found\n")
+
 
 
 commands = {
@@ -38,6 +38,8 @@ commands = {
 def run_command(command, args):
     if command in commands:
         commands[command](args)
+    elif path := shutil.which(command):
+        run_executable(path, args)
     else:
         sys.stdout.write(command + ": command not found\n")
     sys.stdout.flush()
